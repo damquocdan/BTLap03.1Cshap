932c855c9cb9d5f468e3a34448f115128350c429
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-07-28 21:25:55 +0700

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLap03._1Cshap
+{
+    public class Employee
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+        public int yearOfBirth { get; set; }
+        public double salaryLevel { get; set; }
+        public double basicSalary { get; set; }
+
+        public double GetInCome()
+        {
+            double InCome= (salaryLevel * basicSalary);
+            return InCome;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Định danh"+id);
+            Console.WriteLine("Tên:" + name);
+            Console.WriteLine("Năm sinh:" + yearOfBirth);
+            Console.WriteLine("Lương cơ bản:" + basicSalary);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLap03._1Cshap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e= new Employee();
+            e.id = 1;
+            e.name = "Đàm Quốc Dân";
+            e.salaryLevel = 50;
+            e.basicSalary = 300;
+            e.Display();
+            double Income = e.GetInCome();
+            Console.WriteLine("Thu nhập:{0}", Income);
+            Console.ReadKey() ;
+        }
+    }
+}
